8cdad3f51654cfa08c90c79a442153517686defe
Koullou Mohamed <koullou.mohamed@gmail.com>
2019-10-14 19:23:49 +0200

    Modif ajout composants QuoteCard et Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+function Navbar(){
+    return (
+<nav className="Navbar"> 
+    <h1>
+    <a href="https://fr.wikipedia.org/wiki/Les_Simpson">Les Simpsons</a>
+    </h1>
+</nav>
+    );
+}
+export default Navbar
